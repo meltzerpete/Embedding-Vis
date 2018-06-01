3418ff3b520717812c40e82d35e74b7dbd9ef233
pete meltzer <meltzerpete@gmail.com>
2018-06-01 15:49:42 +0100

    heatmap and 2d dimensional reduction vis

---
+++ visualise_embeddings.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import manifold
+
+# Heatmap
+emb = np.loadtxt("/home/pete/needham-deepgl/tests/out.emb", delimiter=" ")
+labels = np.loadtxt("labels.txt", dtype="string")
+
+dist = np.ndarray([len(emb), len(emb)])
+
+for i, e1 in enumerate(emb):
+    for j, e2 in enumerate(emb):
+        dist.itemset((i, j), np.linalg.norm(e1 - e2, 2))
+
+plt.imshow(dist)
+plt.axes().xaxis.tick_top()
+plt.xticks(np.arange(len(dist)), labels)
+plt.yticks(np.arange(len(dist)), labels)
+plt.show()
+
+
+# 2D Visualisation
+# from: https://baoilleach.blogspot.com/2014/01/convert-distance-matrix-to-2d.html
+adist = dist
+amax = np.amax(adist)
+adist /= amax
+
+mds = manifold.MDS(n_components=2, dissimilarity="precomputed", random_state=6)
+results = mds.fit(adist)
+
+coords = results.embedding_
+
+plt.subplots_adjust(bottom = 0.1)
+plt.scatter(
+    coords[:, 0], coords[:, 1], marker = 'o'
+    )
+for label, x, y in zip(labels, coords[:, 0], coords[:, 1]):
+    plt.annotate(
+        label,
+        xy = (x, y), xytext = (-20, 20),
+        textcoords = 'offset points', ha = 'right', va = 'bottom',
+        bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+        arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+
+plt.show()
+
+# plt.plot(emb[0], emb[1], ".")
+# plt.show()
