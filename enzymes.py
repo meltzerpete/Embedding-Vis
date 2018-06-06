ca93b419b15e04fd35551cac284d7d85ef5524fc
pete meltzer <meltzerpete@gmail.com>
2018-06-06 13:02:40 +0100

    added data and code to get edgelist from ENZYMES dataset (https://ls11-www.cs.tu-dortmund.de/staff/morris/graphkerneldatasets)

---
+++ enzymes.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+all_adjacencies = pd.read_csv("ENZYMES/ENZYMES_A.txt", ",", names=["row", "col"])
+all_graph_indicator = pd.read_csv("ENZYMES/ENZYMES_graph_indicator.txt", " ", names=["graph"])
+all_node_labels = pd.read_csv("ENZYMES/ENZYMES_node_labels.txt", " ", names=["label"])
+
+all_adjacencies_with_graph_id = pd.merge(all_adjacencies, all_graph_indicator, left_on="row", right_index=True)
+
+for id in [118, 295, 296]:
+    current = all_adjacencies_with_graph_id[all_adjacencies_with_graph_id.graph == id][["row", "col"]].values
+    np.savetxt(f"ENZYME{id}.edgelist", current, fmt="%d", delimiter=" ")
