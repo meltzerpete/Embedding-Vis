5d7914f1dc6fff4a57bb11f44af90d678027294d
Mark Needham <mark.needham@neotechnology.com>
2018-06-06 16:58:43 +0100

    all the combinations

--- deepgl.py
+++ deepgl.py
@@ -3,15 +3,28 @@ from neo4j.v1 import GraphDatabase
 
 driver = GraphDatabase.driver("bolt://localhost", auth=("neo4j", "neo"))
 
-lambdas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.55, 0.6]
-diffusions = [1, 2, 5, 10]
+# lambdas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.55, 0.6]
+# diffusions = [1, 2, 5, 10]
+
+lambdas = [0.1, 0.4, 0.5]
+diffusions = [2, 5, 10]
 
 with driver.session() as session:
     for l, d in itertools.product(lambdas, diffusions):
         print("Lambda: " + str(l))
+        props = {
+            "lambda": l,
+            "diffusions": d,
+            "writeProperty": "embedding_{0}_{1}".format(l, d)
+        }
         result = session.run("""\
-        call algo.deepgl.stream(null, null, {pruningLambda: $lambda, diffusions: $diffusions})
-        """, {"lambda": l, "diffusions": d})
+        CALL algo.deepgl(null, null, 
+          { pruningLambda: $lambda, 
+            diffusions: $diffusions,
+            writeProperty: $writeProperty
+          }
+        )
+        """, props)
 
         for row in result:
             print(row)
