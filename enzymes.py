56b40d016d990d438246e56012e294fbf0c9ccbf
pete meltzer <meltzerpete@gmail.com>
2018-06-27 09:16:44 +0100

    extract attributes files from enzymes

--- enzymes.py
+++ enzymes.py
@@ -4,12 +4,16 @@ import numpy as np
 all_adjacencies = pd.read_csv("ENZYMES/ENZYMES_A.txt", ",", names=["row", "col"])
 all_graph_indicator = pd.read_csv("ENZYMES/ENZYMES_graph_indicator.txt", " ", names=["graph"])
 all_node_labels = pd.read_csv("ENZYMES/ENZYMES_node_labels.txt", " ", names=["label"])
+all_node_attributes = pd.read_csv("ENZYMES/ENZYMES_node_attributes.txt", ",", header=None)
 
 all_adjacencies_with_graph_id = pd.merge(all_adjacencies, all_graph_indicator, left_on="row", right_index=True)
 
 for id in [118, 295, 296]:
     current = all_adjacencies_with_graph_id[all_adjacencies_with_graph_id.graph == id][["row", "col"]].values
-    np.savetxt(f"ENZYME{id}.edgelist", current, fmt="%d", delimiter=" ")
+    np.savetxt("ENZYME" + str(id) + ".edgelist", current, fmt="%d", delimiter=" ")
     nodes = all_graph_indicator[all_graph_indicator.graph == id]
     labels = nodes.merge(all_node_labels, left_index=True, right_index=True)["label"].reset_index().values
-    np.savetxt(f"ENZYME{id}.labels", labels, fmt="%d", delimiter=" ")
+    np.savetxt("ENZYME" + str(id) + ".labels", labels, fmt="%d", delimiter=" ")
+    # attributes
+    attributes = all_node_attributes.iloc[nodes.index]
+    np.savetxt("ENZYME" + str(id) + ".attributes", attributes, fmt="%f", delimiter=" ")
